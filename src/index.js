495a048d0ba556a0178997b64597edbe2c72b3cb
kehinde cwb <85681159+codewithbillions@users.noreply.github.com>
2023-12-17 03:11:01 +0100

    deployment

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+
+import App from "./App";
+import { store} from "./store/store";
+// import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+    
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+    
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
